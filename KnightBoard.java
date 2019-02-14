60bc67de7ae138e014947200b9b6940073062ee4
DarthChurros <tnarayan00@stuy.edu>
2019-02-14 11:29:07 -0500

    finished moveKnight, wrote movement chart

--- KnightBoard.java
+++ KnightBoard.java
@@ -11,20 +11,19 @@ public class KnightBoard {
   private int[] moveKnight(int r, int c, int direction) {
     if (direction < 0 || direction > 7) throw new IllegalArgumentException("Direction must be from 0 to 7");
     if (!board[r][c]) throw new IllegalStateException("No knight has reached these coordinates");
-    int[] destination = new int[2];
-    destination[0] = r + (int)Math.pow(-1, (direction / 2) % 2) * (direction % 2 + 1);
-    destination[1] = c + (int)Math.pow(-1, (direction / 2 + 1) % 2) * ((direction + 1) % 2 + 1);
-    return destination;
+    r = r + (int)Math.pow(-1, (direction / 2) % 2) * (direction % 2 + 1);
+    c = c + ((int)Math.pow(-1, (direction / 2 + 1) % 2) * ((direction + 1) % 2 + 1) * (int)(Math.pow(-1, direction / 4)));
+    if (board[r][c]) throw new IllegalArgumentException("Point has already been reached");
+    int[] ans = new int[2];
+    ans[0] = r;
+    ans[1] = c;
+    return ans;
   }
 }
 
 /*
-r(0) = 1    c(0) = 2
-r(1) = 2    c(1) = 1
-r(2) = -1   c(2) = -2
-r(3) = -2   c(3) = -1
-r(4) = 1    c(4) = -2
-r(5) = 2    c(5) = -1
-r(6) = -1   c(6) = 2
-r(7) = -2   c(7) = 1
+   3 7
+ 2     6
+ 4     0
+   5 1
 */
