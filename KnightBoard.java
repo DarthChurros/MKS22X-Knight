ac38c1ae39f855c1ea5253c36bf11db07529abe0
DarthChurros <tnarayan00@stuy.edu>
2019-02-15 11:47:29 -0500

    worked on solve

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,22 +1,18 @@
 public class KnightBoard {
-  private boolean[][] board;
-  private int r;
-  private int c;
+  private int[][] board;
   private int[][] outgoing;
 
   public KnightBoard(int r, int c) {
     if (r <= 0 || c <= 0) throw new IllegalArgumentException("Board dimensions must be positive!");
-    board = new boolean[r][c];
+    board = new int[r][c];
     outgoing = new int[r][c];
   }
 
-  private int[] moveKnight(int direction) {
+  private int[] moveKnight(int r, int c, int direction) {
     if (direction < 0 || direction > 7) throw new IllegalArgumentException("Direction must be from 0 to 7");
-    if (!board[r][c]) throw new IllegalStateException("No knight has reached these coordinates");
+    if (board[r][c] == 0) throw new IllegalStateException("No knight has reached these coordinates");
     r = r + (int)Math.pow(-1, (direction / 2) % 2) * (direction % 2 + 1);
     c = c + ((int)Math.pow(-1, (direction / 2 + 1) % 2) * ((direction + 1) % 2 + 1) * (int)(Math.pow(-1, direction / 4)));
-    if (board[r][c]) throw new IllegalArgumentException("Point has already been reached");
-    board[r][c] = true;
     int[] ans = new int[2];
     ans[0] = r;
     ans[1] = c;
@@ -26,12 +22,24 @@ public class KnightBoard {
   public boolean solve(int startR, int startC) {
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j]) throw new IllegalStateException("Board must be empty to solve!");
+        if (board[i][j] != 0) throw new IllegalStateException("Board must be empty to solve!");
       }
     }
-    r = startR;
-    c = startC;
-    board[r][c] = true;
+    return solve(1, startR, startC);
+  }
+
+  private boolean solve(int moves, int r, int c) {
+    board[r][c] = moves;
+    if (moves >= board.length * board[r].length) return true;
+    if (r < 0 || r >= board.length || c < 0 || c >= board[r].length) {
+      board[r][c] = 0;
+      return false;
+    }
+    for (int i = 0; i < 8; i++) {
+      System.out.println(this);
+      if (solve(moves + 1, moveKnight(r, c, i)[0], moveKnight(r, c, i)[1])) return true;
+    }
+    board[r][c] = 0;
     return false;
   }
 
@@ -39,12 +47,8 @@ public class KnightBoard {
     String ans = "";
     for (int i = 0; i < board.length; i++) {
       for (int j = 0; j < board[i].length; j++) {
-        if (board[i][j]) {
-          if (r == i && c == j) {
-            ans += "N ";
-          } else {
-            ans += "X ";
-          }
+        if (board[i][j] != 0) {
+          ans += board[i][j] + " ";
         } else {
           ans += "_ ";
         }
@@ -55,15 +59,8 @@ public class KnightBoard {
   }
 
   public static void main(String[] args) {
-    KnightBoard test = new KnightBoard(5, 5);
-    System.out.println(test);
-    test.moveKnight(4);
-    System.out.println(test);
-    test.moveKnight(2);
-    System.out.println(test);
-    test.moveKnight(1);
-    System.out.println(test);
-    test.moveKnight(5);
+    KnightBoard test = new KnightBoard(3, 3);
+    System.out.println(test.solve(0, 0));
     System.out.println(test);
 
   }
